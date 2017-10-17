5ac941930c31080394930e2fd6d4dc22cf1c927e
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2017-10-17 22:40:16 +0300

    upgrade to Spring Framework 5.0.0

--- src/main/java/example/config/ExampleWebMvcConfiguration.java
+++ src/main/java/example/config/ExampleWebMvcConfiguration.java
@@ -1,17 +1,14 @@
 package example.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
 import java.util.List;
@@ -19,7 +16,7 @@ import java.util.List;
 @EnableWebMvc
 @Import(SerializationConfig.class)
 @ComponentScan("example.controllers")
-public class ExampleWebMvcConfiguration extends WebMvcConfigurerAdapter {
+public class ExampleWebMvcConfiguration implements WebMvcConfigurer {
 
     private final ObjectMapper objectMapper;
 
